474a298ea0c4584752a788f8bda5035229dd0dfb
netolobo <desilio@gmail.com>
2023-10-20 13:20:46 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Neto Lobo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
